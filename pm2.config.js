b78e0fd91a23c5f580b88605a71b595003627712
sourav-js <76809746+sourav-js@users.noreply.github.com>
2022-03-07 02:20:15 +0530

    Update pm2.config.js

--- pm2.config.js
+++ pm2.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     apps : [{
-      name   : "adminPanel",
+      name   : "groceryji",
       script : "nodemon grocery.js",
-      error_file : "/home/sourav/sourav/admingroceryJi_error.log",
-      out_file : "/home/sourav/sourav/admingroceryJi_out.log"
+      error_file : "/home/sourav/sourav/groceryJi_error.log",
+      out_file : "/home/sourav/sourav/groceryJi_out.log"
     }]
   }
